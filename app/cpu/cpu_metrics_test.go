7f450c628197290185832dce6897850db53cc89f
Ayoubyte <ayoubboubkrioui2@gmail.com>
2023-08-12 12:11:32 +0100

    pushing cpu metrics unit testing while testing jenkins pipeline

--- app/cpu/cpu_metrics_test.go
+++ app/cpu/cpu_metrics_test.go
@@ -1,57 +1,57 @@
 package cpu
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/prometheus/client_golang/prometheus"
-// )
+	"github.com/prometheus/client_golang/prometheus"
+)
 
-// func TestRegisterCpuMetrics(t *testing.T) {
-// 	cpuMetrics := RegisterCpuMetrics()
+func TestRegisterCpuMetrics(t *testing.T) {
+	cpuMetrics := RegisterCpuMetrics()
 
-// 	// Create a new registry to register metrics
-// 	reg := prometheus.NewRegistry()
-// 	reg.MustRegister(cpuMetrics.CpuUsage)
-// 	reg.MustRegister(cpuMetrics.CpuFree)
-// 	reg.MustRegister(cpuMetrics.CpuUsagePerApp)
+	// Create a new registry to register metrics
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(cpuMetrics.CpuUsage)
+	reg.MustRegister(cpuMetrics.CpuFree)
+	reg.MustRegister(cpuMetrics.CpuUsagePerApp)
 
-// 	// Verify if the metrics are registered
-// 	_, err := reg.Gather()
-// 	if err != nil {
-// 		t.Fatalf("failed to gather metrics: %v", err)
-// 	}
+	// Verify if the metrics are registered
+	_, err := reg.Gather()
+	if err != nil {
+		t.Fatalf("failed to gather metrics: %v", err)
+	}
+}
+
+// type mockCPUMetrics struct {
+// 	cpuUsage float64
+// 	cpuFree  float64
 // }
 
-// // type mockCPUMetrics struct {
-// // 	cpuUsage float64
-// // 	cpuFree  float64
-// // }
+// func (m *mockCPUMetrics) CpuUsage() prometheus.Gauge {
+// 	return prometheus.NewGauge(prometheus.GaugeOpts{})
+// }
 
-// // func (m *mockCPUMetrics) CpuUsage() prometheus.Gauge {
-// // 	return prometheus.NewGauge(prometheus.GaugeOpts{})
-// // }
+// func (m *mockCPUMetrics) CpuFree() prometheus.Gauge {
+// 	return prometheus.NewGauge(prometheus.GaugeOpts{})
+// }
 
-// // func (m *mockCPUMetrics) CpuFree() prometheus.Gauge {
-// // 	return prometheus.NewGauge(prometheus.GaugeOpts{})
-// // }
+// func TestCollectCpuMetrics(t *testing.T) {
+// 	estMetrics := &CPUMetrics{
+// 		CpuUsage: NewTestGauge(),
+// 		CpuFree:  NewTestGauge(),
+// 	}
 
-// // func TestCollectCpuMetrics(t *testing.T) {
-// // 	estMetrics := &CPUMetrics{
-// // 		CpuUsage: NewTestGauge(),
-// // 		CpuFree:  NewTestGauge(),
-// // 	}
+// 	done := make(chan struct{})
+// 	defer close(done)
 
-// // 	done := make(chan struct{})
-// // 	defer close(done)
+// 	go cpu.CollectCpuMetrics(testMetrics, done)
 
-// // 	go cpu.CollectCpuMetrics(testMetrics, done)
+// 	// Let the function run for a short time
+// 	time.Sleep(time.Second)
 
-// // 	// Let the function run for a short time
-// // 	time.Sleep(time.Second)
+// 	// Add your assertions here to check if metrics are being updated correctly
+// 	assert.NotNil(t, testMetrics.CpuUsage)
+// 	assert.NotNil(t, testMetrics.CpuFree)
 
-// // 	// Add your assertions here to check if metrics are being updated correctly
-// // 	assert.NotNil(t, testMetrics.CpuUsage)
-// // 	assert.NotNil(t, testMetrics.CpuFree)
-
-// // 	done <- struct{}{} // Stop the goroutine
-// // }
+// 	done <- struct{}{} // Stop the goroutine
+// }
